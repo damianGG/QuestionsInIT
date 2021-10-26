46d131ea19f0cc91dd3c452fe4e4ee157f6fb93f
Damian <damiangolon@gmail.com>
2021-10-27 00:23:15 +0200

    Added google tagmanager

--- src/index.js
+++ src/index.js
@@ -4,21 +4,14 @@ import {Provider} from 'react-redux';
 import {createStore,applyMiddleware,compose} from 'redux';
 import thunk from 'redux-thunk'
 import './index.css';
-import TagManager from 'react-gtm-module'
 import reducers from './reducers';
 import App from './App';
 import {AppBar,Typography,Container}
 from '@material-ui/core';
-import Navbar from './components/Header/Navbar';
+
 
 
 const store = createStore(reducers,compose(applyMiddleware(thunk))); 
-const tagManagerArgs = {
-    gtmId: 'G-73BCM11PKV'
-}
- 
-TagManager.initialize(tagManagerArgs)
-
 
 ReactDOM.render(
             <Provider store ={store}>
